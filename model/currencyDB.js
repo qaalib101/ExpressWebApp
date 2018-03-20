db021e630267c85690e11586e8144a1d675bb0b3
qaalib101 <si8822fb@go.minneapolis.edu>
2018-03-19 22:28:18 -0500

    oSNFOF

--- model/currencyDB.js
+++ model/currencyDB.js
@@ -2,6 +2,13 @@
  * Created by si8822fb on 3/13/2018.
  */
 module.exports = {
-  EUR: 0.85,
-    JPY:112.63
+  USDtoEUR: 0.85,
+    USDtoJPY:112.63,
+    USDtoUSD: 1,
+    EURtoUSD:1.23,
+    EURtoJPY:131.10,
+    EURtoEUR:1,
+    JPYtoEUR:0.01,
+    JPYtoUSD:0.01,
+    JPYtoJPY:1
 };
--- routes/index.js
+++ routes/index.js
@@ -6,20 +6,28 @@ var exchangeRates = require('../model/currencyDB');
 router.get('/', function(req, res, next) {
   res.render('index');
 });
+/*Get the conversion result page*/
 router.get('/convert', function(req, res, next) {
-  var dollars = req.query.dollars;
-  var rate = req.query.to_currency;
-
-  var converted = dollars* exchangeRates[rate];
+  var currency_amount = req.query.currency_amount;
+  var to_rate = req.query.to_currency;
+  var from_rate = req.query.from_currency;
+  var fromCurrency = from_rate.textContent;
+  var rate = to_rate.toString() + "to" + from_rate.toString();
+  var converted = currency_amount * exchangeRates[rate];
 
   res.render('results', {
-    dollars: dollars,
-      toCurrency: rate,
+    currency: currency_amount,
+      type: fromCurrency,
+      toCurrency: to_rate,
       converted:converted
   });
 });
+/*get the about page*/
 router.get('/about', function(req, res, next){
-  res.render('about', {name: " this work"})
+  res.render('about', {
+    name: "Qaalib Farah",
+      description: "This site is used to convert US " +
+      "dollars into foreign currency"})
 });
 
 module.exports = router;
