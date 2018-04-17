46950f148e88fa2e777e1ed00c7a370d518bda3c
qaalib101 <si8822fb@go.minneapolis.edu>
2018-04-16 23:19:35 -0500

    oSNFOF

--- model/currencyDB.js
+++ model/currencyDB.js
@@ -1,17 +1,33 @@
-/**
- * Created by si8822fb on 3/13/2018.
- */
-module.exports = {
-    // usd rates
-  USDtoEUR: 0.85,
-    USDtoJPY:112.63,
-    USDtoUSD: 1,
-    // eur rates
-    EURtoUSD:1.23,
-    EURtoJPY:131.10,
-    EURtoEUR:1,
-    // jpy rates
-    JPYtoEUR:0.01,
-    JPYtoUSD:0.01,
-    JPYtoJPY:1
-};
+var request = require('request');
+
+var baseURL = 'http://api.fixer.io/latest';
+
+function currencyRequest(callback, base, to) {
+
+    process.nextTick(function() {   // This forces this call to wait until the current task
+                                    //has finished, so it's doesn't block any other requests.
+                                    //When it's done, it will use the callback to notify the caller that it's done.
+
+        // TODO include your API key in the query parameters
+        queryParam = { 'base': base,  'symbols': to, 'access_key': 'e01970f33b001da0927c8c7dfa13fea1'};
+
+        //Use request module to request currency data from Fixer API
+        //Must handle result in callback. Can't return data from an asychronous function.
+        request( { uri: baseURL, qs: queryParam } , function(error, fixer_response, body) {
+
+            if (!error && fixer_response.statusCode == 200){
+                console.log("FIXER SAYS \n" + JSON.stringify(body));
+                var fixerJSON = JSON.parse(body);   //Convert JSON text to a JavaScript object
+                callback(null, fixerJSON);
+            }
+
+            else {
+              console.log("Error in JSON request: " + error);
+                console.log(fixer_response);
+                callback(Error("Error fetching data from the fixer service"));
+            }
+        });
+    });
+}
+
+module.exports = currencyRequest;
